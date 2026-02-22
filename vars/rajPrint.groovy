75fd6a472133623bb7078e99227cb0ef538aa506
Pb8123 <praveenrathnakar8@gmail.com>
2026-02-22 18:32:47 +0000

    Update rajPrint.groovy

--- vars/rajPrint.groovy
+++ vars/rajPrint.groovy
@@ -1,12 +1,18 @@
+def call() 
+{ 
+  rajcall() 
+  novacall() 
+  benjicall() 
+} 
 def benjicall() 
-{
+{ 
   echo "Hello benji!" 
-}
+} 
 def rajcall() 
 {
   echo "Hello raj!" 
-}
+} 
 def novacall() 
-{
+{ 
   echo "echo nova!" 
 }
